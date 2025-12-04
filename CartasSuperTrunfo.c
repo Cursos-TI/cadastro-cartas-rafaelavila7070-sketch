bd00d3872a77ac4c83029c46f74cd83a27e4ad8a
rafaelavila7070-sketch <rafaelavila7070@gmail.com>
2025-12-04 14:23:38 +0000

    Criei um jogo de super trunfu de cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,30 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+char nome[50]; 
+    int população;
+    float area;
+    float PIB;
+    int NPT;
+    
+printf("Digite o nome da cidade: \n");
+fgets(nome,50,stdin);
 
-  // Área para entrada de dados
+printf("Digite a população: \n");
+scanf("%d", &população);
 
-  // Área para exibição dos dados da cidade
+printf("Digite a área: \n");
+scanf("%f", &area);
+
+printf("Digite o PIB: \n");
+scanf("%f", &PIB);
+
+printf("Digite o número de pontos turísticos: \n");
+scanf("%d", &NPT);
+
+printf("Nome da cidade: %s\n População: %d\n Área: %f\n", nome, população, area);    
+printf("PIB: %f\n Número de pontos turísticos: %d\n", PIB, NPT);
+
 
 return 0;
 } 
